ff80fd2fa8d952c7bc9ad26972445ea5d155a77e
jcp0997 <jpenas1997@gmail.com>
2024-03-25 11:35:57 -0300

    Codigo Selenium para Amazon

---
+++ UI-Test-Amazon-Java-Cucumber-Selenium-master/src/test/java/pages/AmazonPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+
+public class AmazonPage extends BasePage {
+    
+    public AmazonPage(){
+        super(driver);
+    }
+
+    private String searchField = "//input[@id='twotabsearchtextbox']";
+    private String searchButton = "//input[@id='nav-search-submit-button']";
+    private String pickThirdItem2="//div[@class='sg-col-inner']//div[@class='a-section a-spacing-small a-spacing-top-small']//div[@class='a-section a-spacing-none puis-padding-right-small s-title-instructions-style']//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']";
+    private String addToCart = "//input[@id='add-to-cart-button-ubb']";
+    private String addToCartMsg = "//span[contains(text(),'Agregado al carrito')]";
+    
+    
+    public void navigateToAmazon(){
+        navigateTo("https://www.amazon.com/");
+    }
+
+    public void enterSearchCriteria (String criteria){
+        write(searchField, criteria);
+    }
+
+    public void clickSearchButon(){
+        clickElement(searchButton);
+    }
+
+    public void goToPage(String pageNumber){
+        goToLinkText(pageNumber);
+    }
+
+    public void selectThirdItem2(int index){
+        selectItems(pickThirdItem2,index);
+    }
+
+    public void addItemToCart(){
+        clickElement(addToCart);
+        }
+
+    public String addToCartMessage(){
+        return textFromElement(addToCartMsg);
+    }
+
+
+}
---
+++ UI-Test-Amazon-Java-Cucumber-Selenium-master/src/test/java/pages/BasePage.java
@@ -0,0 +1,74 @@
+package pages;
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class BasePage {
+
+    protected static WebDriver driver;
+    private static WebDriverWait wait;
+    //private static Actions action;
+    
+ 
+    static{
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        ChromeOptions chromeoptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeoptions);
+        wait = new WebDriverWait(driver,30);
+    }
+
+    public BasePage(WebDriver driver){
+        BasePage.driver = driver;
+        wait = new WebDriverWait(driver,30);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    public static void navigateTo(String url){
+        driver.get(url);
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
+    
+    private WebElement Find (String locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator))); 
+    }
+
+    public void clickElement (String locator){
+        Find(locator).click();
+    }
+
+    public void write (String locator, String textToWrite){
+        Find(locator).clear();
+        Find(locator).sendKeys(textToWrite);
+    }
+
+    public void goToLinkText(String link){
+        driver.findElement(By.linkText(link)).click();;
+    }
+
+    public String textFromElement(String locator){
+        return Find(locator).getText();
+    }
+
+    public void selectItems(String locator, int index){
+        List<WebElement> results = driver.findElements(By.xpath(locator));
+        results.get(index).click();
+        }
+
+
+
+
+
+}
